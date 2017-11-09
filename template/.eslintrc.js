086fc59ccec6b3eb966387c89eb1e34c40e91464
Sergey Rudachenko <m1no.serg@gmail.com>
2017-11-09 15:48:44 +0300

    more eslint rules

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -24,9 +24,13 @@ module.exports = {
     "eol-last": [ "error", "always" ],
     "quotes": [ "error", "single" ],
     "no-extra-parens": [ "error" ],
+    "func-style": [ "error", "declaration" ],
+    "prefer-arrow-callback": [ "error", { "allowNamedFunctions": false, "allowUnboundThis": false } ],
     "no-unsafe-negation": [ "error" ],
     "no-alert": [ "warn" ],
+    "object-shorthand": [ "error", "always" ],
     "no-console": [ "warn" ],
+    "no-var": [ "error" ],
     "semi": [ "error", "always" ],
     "camelcase": [ "error", { "properties": "never" } ],
     "dot-notation": [ "error" ],
@@ -40,6 +44,37 @@ module.exports = {
     "space-before-function-paren": [ "error", "never" ],
     "array-bracket-spacing": [ "error", "always" ],
     "key-spacing": [ "error" ],
-    "vue/attribute-hyphenation": [ 2, "always" ]
+    "space-before-blocks": [ "error", "always" ],
+    "vue/attribute-hyphenation": [ 2, "always" ],
+    "vue/no-side-effects-in-computed-properties": [ "error" ],
+    "vue/html-end-tags": [ "error" ],
+    "vue/v-bind-style": [ "error" ],
+    "vue/no-multi-spaces": [ "error" ],
+    "comma-spacing":  ["error", { "before": false, "after": true }],
+    "vue/no-reserved-keys": [ "error" ],
+    "vue/return-in-computed-property": [ "error" ],
+    "vue/mustache-interpolation-spacing": [ 2, "always" ],
+    "vue/v-on-style": [ "error" ],
+    "vue/order-in-components": [2, {
+      "order": [
+        "mixins",
+        "components",
+        "props",
+        "beforeCreate",
+        "created",
+        "beforeMount",
+        "mounted",
+        "beforeUpdate",
+        "updated",
+        "activated",
+        "deactivated",
+        "beforeDestroy",
+        "destroyed",
+        "data",
+        "computed",
+        "watch",
+        "methods"
+      ]
+    }]
   }
 };
